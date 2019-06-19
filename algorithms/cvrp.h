3bd9d63b662794ecb8c87ab9fa673d3dd974137a
nikfadda <nikfadda@gmail.com>
2019-06-19 09:50:29 +0200

    Nuovi metodi di calcolo delle tabelle

--- algorithms/cvrp.h
+++ algorithms/cvrp.h
@@ -8,6 +8,16 @@
 #include <data_structures/routes.h>
 
 std::array<size_t,2> getMaxIndexes(cg3::Array2D<double> matrix);
+
+void bootRoute(const Topology& topology);
+void growRoute(const Topology& topology);
+
+cg3::Array2D<double>& computeDistTable(std::vector<Node>& nodes);
+cg3::Array2D<double>& computeSaveTable(cg3::Array2D<double>& distTable);
+
 void cWseq(const Topology& topology);
+void cWpar(const Topology& topology);
+
+
 
 #endif // CVRP_H
