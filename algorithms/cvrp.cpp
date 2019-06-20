47ca04311bd9706778ff800eda2869bb4c2340b2
nikfadda <nikfadda@gmail.com>
2019-06-20 09:53:21 +0200

    Cambiamenti minori

--- algorithms/cvrp.cpp
+++ algorithms/cvrp.cpp
@@ -28,13 +28,16 @@ size_t findBestBackhaulSuccessorFromLinehaul(Node& linehaul,std::vector<Node>& b
 
     size_t maxIndex = 0;
     double maxSaving = 0.0;
+    double saving = 0.0;
+    double distLinehaulBackhaul, distLinehaulFromStart, distBackhaulFromStart;
 
     for (size_t i = 1;i < backhauls.size();i++) {
+
         Node& currentBackhaul = backhauls[i];
-        double saving = 0.0;
-        double distLinehaulBackhaul = linehaul.getCoordinates().dist(currentBackhaul.getCoordinates());
-        double distLinehaulFromStart = linehaul.getCoordinates().dist(backhauls[0].getCoordinates());
-        double distBackhaulFromStart = currentBackhaul.getCoordinates().dist(backhauls[0].getCoordinates());
+        saving=0.0;
+        distLinehaulBackhaul = linehaul.getCoordinates().dist(currentBackhaul.getCoordinates());
+        distLinehaulFromStart = linehaul.getCoordinates().dist(backhauls[0].getCoordinates());
+        distBackhaulFromStart = currentBackhaul.getCoordinates().dist(backhauls[0].getCoordinates());
 
         saving = distBackhaulFromStart + distLinehaulFromStart - distLinehaulBackhaul;
 
@@ -47,7 +50,7 @@ size_t findBestBackhaulSuccessorFromLinehaul(Node& linehaul,std::vector<Node>& b
     return maxIndex;
 }
 
-void eraseFromSaveTableByIndex(std::list<std::array<size_t,2>>& saveList,size_t index){
+void eraseFromSaveListByItem(std::list<std::array<size_t,2>>& saveList,size_t index){
 
     std::list<std::array<size_t,2>>::iterator it = saveList.begin();
 
@@ -64,23 +67,26 @@ void eraseFromSaveTableByIndex(std::list<std::array<size_t,2>>& saveList,size_t 
 //data una savingList e un indice aggiunge alla route il successore con il saving più alto(se esistente),elimina le coppie che includono il predecessore e
 //restituisce true se va a buon fine o false viceversa
 
-bool addBestAdjacentNodeByIndex(const std::vector<Node> nodeList,std::list<std::array<size_t,2>>& saveList,std::vector<Node>& route,const size_t index,
+bool addBestAdjacentNodeByIndex(const std::vector<Node> nodeList,std::list<std::array<size_t,2>>& saveList,
+                                std::vector<Node>& route, const size_t index,
                                 const double& max_capacity,double& current_capacity,const bool lineHaulOrBackhaul){
 
 
     std::list<std::array<size_t,2>>::iterator it = saveList.begin();
+    size_t candidateIndex;
+    Node otherNode;
 
     //itero la lista
     while(it != saveList.end()){
         //se l'attuale coppia di indici include l'indice di cui cerco il successore:
         if((*it)[0] == index || (*it)[1] == index){
-            size_t candidateIndex;
+
             //assegno a candidateIndex l'indice del successore
             if((*it)[0] == index) candidateIndex = (*it)[1];
             else candidateIndex = (*it)[0];
 
             //controllo se l'altro nodo ha valori di delivery o pickup soddisfabili:
-            Node otherNode = nodeList[candidateIndex];
+            otherNode = nodeList[candidateIndex];
             if(lineHaulOrBackhaul){
                 //per i Linehaul
                 if(otherNode.getDelivery() > current_capacity){
@@ -90,7 +96,7 @@ bool addBestAdjacentNodeByIndex(const std::vector<Node> nodeList,std::list<std::
                 else {
                     //se si:
                     //elimino tutte le coppie che includono il predecessore
-                    eraseFromSaveTableByIndex(saveList,index);
+                    eraseFromSaveListByItem(saveList,index);
                     //aggiungo il successore alla route
                     current_capacity -= otherNode.getDelivery();
                     route.push_back(otherNode);
@@ -106,7 +112,7 @@ bool addBestAdjacentNodeByIndex(const std::vector<Node> nodeList,std::list<std::
                 else {
                     //se si:
                     //elimino tutte le coppie che includono il predecessore
-                    eraseFromSaveTableByIndex(saveList,index);
+                    eraseFromSaveListByItem(saveList,index);
                     //aggiungo il successore alla route
                     current_capacity -= otherNode.getPickup();
                     route.push_back(otherNode);
@@ -176,51 +182,65 @@ void computeSaveList(cg3::Array2D<double>& saveTable, std::list<std::array<size_
 void cWseq(const Topology& topology){
 
     size_t nNodesLinehaul=topology.getLinehaulNodes().size();
+
     //inizializzo tabella distanze Linehaul
     cg3::Array2D<double> distTableLinehaul = cg3::Array2D<double>(nNodesLinehaul,nNodesLinehaul,0.0);
     computeDistTable(topology.getLinehaulNodes(),distTableLinehaul);
+
     //inizializzo tabella savings Linehaul
     cg3::Array2D<double> saveTableLinehaul = cg3::Array2D<double>(nNodesLinehaul,nNodesLinehaul,0.0);
     computeSaveTable(distTableLinehaul, saveTableLinehaul);
+
     //inizializzo lista ordinata savings Linehaul
     std::list<std::array<size_t,2>> saveListLinehaul;
     computeSaveList(saveTableLinehaul,saveListLinehaul);
 
 
     size_t nNodesBackhaul=topology.getBackhaulNodes().size();
+
     //inizializzo tabella distanze Backhaul
     cg3::Array2D<double> distTableBackhaul = cg3::Array2D<double>(nNodesBackhaul,nNodesBackhaul,0.0);
     computeDistTable(topology.getBackhaulNodes(),distTableBackhaul);
+
     //inizializzo tabella distanze Backhaul
     cg3::Array2D<double> saveTableBackhaul = cg3::Array2D<double>(nNodesBackhaul,nNodesBackhaul,0.0);
     computeSaveTable(distTableBackhaul, saveTableBackhaul);
+
     //inizializzo tabella distanze Backhaul
     std::list<std::array<size_t,2>> saveListBackhaul;
     computeSaveList(saveTableBackhaul,saveListBackhaul);
 
+    //Processo del passo base
     Routes routes;
     std::vector<Node> tmpRoute;
+    size_t lastNodeAdded;
+    double current_capacity;
+    std::array<size_t,2> nodeCouple;
+    bool hasNotFailed;
 
     for (int i=0;i<topology.getVehicle_num();i++) {
 
-        size_t lastNodeAdded;
-
-        double current_capacity = topology.getCapacity();
+        current_capacity = topology.getCapacity();
 
         if(saveListLinehaul.size() > 0){
+
             //aggiungo alla route i primi due Linehaul con il saving più alto
-            std::array<size_t,2> nodeCouple = saveListLinehaul.front();
+            nodeCouple = saveListLinehaul.front();
             saveListLinehaul.erase(saveListLinehaul.begin());
             tmpRoute.push_back(topology.getLinehaulNodes()[nodeCouple[0]]);
             tmpRoute.push_back(topology.getLinehaulNodes()[nodeCouple[1]]);
         }
+
         //segnala se il veicolo ha capacità sufficiente a soddisfare il delivery del successore ottimale(saving più alto) nella route
-        bool hasNotFailed = true;
+        hasNotFailed = true;
 
         while(hasNotFailed){
             lastNodeAdded = tmpRoute.back().getIndex();
+
             //prova ad aggiungere il miglior successore e aggiorna hasNotFailed con true se è riuscito o false se ha fallito
-            hasNotFailed = addBestAdjacentNodeByIndex(topology.getLinehaulNodes(),saveListLinehaul,tmpRoute,lastNodeAdded,topology.getCapacity(),current_capacity,true);
+            hasNotFailed = addBestAdjacentNodeByIndex(topology.getLinehaulNodes(),saveListLinehaul,
+                                                      tmpRoute,lastNodeAdded,topology.getCapacity(),
+                                                      current_capacity,true);
         }
 
 
